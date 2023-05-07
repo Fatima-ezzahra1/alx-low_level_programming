4e362a303032851f64eb3c6430d0c7302c2137aa
Fatima-ezzahra <fatimaezzahra.iaiche@gmail.com>
2023-05-07 07:41:03 -0700

    Task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
-  *print_alphabet() - print all alphabet in lowercase
+  *main - Entry point
+  *Description:  print_alphabet() - print all alphabet in lowercase
   */
 void print_alphabet(void)
 {
-	int i;
+	char i;
 
 	for (i = 97; i <= 122; i++)
-	{
 		_putchar(i);
-	}
 	_putchar('\n');
 }
