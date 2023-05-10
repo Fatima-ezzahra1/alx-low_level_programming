ca733ba13ffc2a2e6ca9eb4800a0c21fc868f43e
Fatima-ezzahra <fatimaezzahra.iaiche@gmail.com>
2023-05-10 15:09:47 -0700

    task2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * _strlen_recursion - returns the length of a string
+  * @s: input
+  * Return: Always 0
+  */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+	return (len);
+}
