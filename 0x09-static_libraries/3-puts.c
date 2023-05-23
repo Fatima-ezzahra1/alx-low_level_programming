0acca49f93515a5834691161fc36081d2311bd6a
Fatima-ezzahra <fatimaezzahra.iaiche@gmail.com>
2023-05-23 12:30:10 -0700

    Task0

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - Prints a string to the standard output
+ * @str: The string to be printed
+ * Return: None
+ */
+void _puts(char *str)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
