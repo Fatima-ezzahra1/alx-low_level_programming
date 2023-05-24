f97e0beacd33d498bc69618e751d1543cd1303f8
Fatima-ezzahra <fatimaezzahra.iaiche@gmail.com>
2023-05-24 05:16:02 -0700

    Task3

---
+++ 0x0F-function_pointers/3-get_op-func.c
@@ -0,0 +1,23 @@
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+ * get_op_func - select the correct asken function to perform
+ * @s: The argument
+ * Return: A pointer to the function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
+	};
+	int a = 0;
+
+	while (ops[a].op != NULL && *(ops[a].op) != *s)
+		a++;
+	return (ops[a].f);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add - Returns the sum of number 1 and 2
+ * @a: number 1
+ * @b: number 2
+ * Return: The sum of a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Returns the difference of number 1 and 2
+ * @a: number 1
+ * @b: number 2
+ * Return: The difference of a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Returns the product of number 1 and 2
+ * @a: number 1
+ * @b: number 2
+ * Return: The product of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Returns the division of number 1 and 2
+ * @a: number 1
+ * @b: number 2
+ * Return: The quotient of a and b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Returns the remainder of the division of number 1 and 2
+ * @a: number 1
+ * @b: number 2
+ * Return: The remainder of the division of a by b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
